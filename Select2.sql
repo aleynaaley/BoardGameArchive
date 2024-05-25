ed022222cf442f71cbd46c0170bf8966cc2a3051
Zeynep TATAR <55032271+zeynep-pixel@users.noreply.github.com>
2024-05-26 00:14:16 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -6,20 +6,4 @@ JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
 JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
 GROUP BY tblUye.UYE_ID, tblUye.AD_Üye , tblUye.SOYAD_Üye ;
 
-// EĞER OYUNA SAHİP OLMA TARİHİ NULL DEĞİLSE EKLE
 
-SELECT 
-    tblUye.UYE_ID, 
-    tblUye.AD_Üye, 
-    tblUye.SOYAD_Üye,   
-    COUNT(CASE WHEN tbloyun_koleksiyon.OYUN_SAHIP_OLMA_TARIHI IS NOT NULL THEN tbloyun_koleksiyon.OYUN_ID END) as Oyun_Sayısı 
-FROM 
-    tblUye
-JOIN 
-    tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
-JOIN 
-    tbloyun_koleksiyon ON tblKoleksiyon.KOLEKSIYON_ID = tbloyun_koleksiyon.KOLEKSIYON_ID
-GROUP BY 
-    tblUye.UYE_ID, 
-    tblUye.AD_Üye, 
-    tblUye.SOYAD_Üye;
