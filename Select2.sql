66d61b63ebeee34b996047da09c92fb0930a38af
Zeynep TATAR <55032271+zeynep-pixel@users.noreply.github.com>
2024-05-26 01:17:04 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -1,11 +1,32 @@
 
 
-SELECT tblUye.UYE_ID, tblUye.AD_Üye, tblUye.SOYAD_Üye , COUNT(DISTINCT tblOyuna_puan_verir.ID) as Puan_Verme_Sayisi
-FROM tblUye
-LEFT JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
-LEFT JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
-GROUP BY tblUye.UYE_ID, tblUye.AD_Üye , tblUye.SOYAD_Üye ;
-
-
+SELECT 
+    tblUye.UYE_ID, 
+    tblUye.AD_Üye, 
+    tblUye.SOYAD_Üye, 
+    Koleksiyon_Sayısı,
+    Puan_Verme_Sayısı
+FROM 
+    tblUye
+LEFT JOIN (
+    SELECT 
+        UYE_ID,
+        COUNT(KOLEKSIYON_ID) AS Oyun_Sayısı
+    FROM 
+        tblKoleksiyon
+    GROUP BY 
+        UYE_ID
+) AS KoleksiyonSayisi ON tblUye.UYE_ID = KoleksiyonSayisi.UYE_ID
+LEFT JOIN (
+    SELECT 
+        tblKoleksiyon.UYE_ID,
+        COUNT(DISTINCT tblOyuna_puan_verir.ID) AS Puan_Verme_Sayısı
+    FROM 
+        tblKoleksiyon
+    LEFT JOIN 
+        tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
+    GROUP BY 
+        tblKoleksiyon.UYE_ID
+) AS PuanVermeSayisi ON tblUye.UYE_ID = PuanVermeSayisi.UYE_ID;
 
 
