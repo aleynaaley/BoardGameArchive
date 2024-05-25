bfd42421f8419833a961fa00459f6c7a08a5b4df
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-05-26 00:05:02 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -24,3 +24,31 @@ LEFT JOIN tbluye_oyun_oynar uoo ON uye.UYE_ID = uoo.UYE_ID
 LEFT JOIN tblOyun oyun ON uoo.OYUNSEANS_ID = os.OYUNSEANS_ID AND os.oyun_ID = oyun.OYUN_ID
 
 GROUP BY uye.AD_Üye, uye.SOYAD_Üye, ulke.ULKE_AD;
+
+
+
+
+
+
+
+---------------------
+
+USE boardgame;
+
+-- Üyelerin adı, soyadı, ülkesi, sahip olduğu ana oyun sayısı, sahip olduğu eklenti paketi sayısı,
+-- kaç oyuna puan verdiği, kaç oyun oturumu oluşturduğu ve kaç defa "Ticket to Ride" isimli oyunu oynadığı bilgilerini getiren sorgu.
+SELECT 
+    u.AD_Üye AS Ad,
+    u.SOYAD_Üye AS Soyad,
+    ulke.ULKE_AD AS Ulke,
+    (SELECT COUNT(*) FROM tbloyun o WHERE o.KOLEKSIYON_ID IN (SELECT k.KOLEKSIYON_ID FROM tblKoleksiyon k WHERE k.UYE_ID = u.UYE_ID) AND o.EKLENTI_PAKETI IS NULL) AS AnaOyunSayisi,
+    (SELECT COUNT(*) FROM tbloyun o WHERE o.KOLEKSIYON_ID IN (SELECT k.KOLEKSIYON_ID FROM tblKoleksiyon k WHERE k.UYE_ID = u.UYE_ID) AND o.EKLENTI_PAKETI IS NOT NULL) AS EklentiPaketiSayisi,
+    (SELECT COUNT(*) FROM tblOyuna_puan_verir opv WHERE opv.UYE_ID = u.UYE_ID) AS PuanVerdigiOyunSayisi,
+    (SELECT COUNT(*) FROM tblOyun_seanslar os WHERE os.OLUSTURAN_UYE_ID = u.UYE_ID) AS OlusturduguOyunOturumSayisi,
+    (SELECT COUNT(*) FROM tbluye_oyun_oynar uoo
+     JOIN tblOyun_seanslar os ON uoo.OYUNSEANS_ID = os.OYUNSEANS_ID
+     JOIN tbloyun o ON os.OYUN_ID = o.OYUN_ID
+     WHERE uoo.UYE_ID = u.UYE_ID AND o.OYUN_ASILAD = 'Ticket to Ride') AS TicketToRideOynamaSayisi
+FROM tblUye u
+JOIN tblUlke ulke ON u.ULKE_ID = ulke.ULKE_ID
+ORDER BY u.AD_Üye, u.SOYAD_Üye;
