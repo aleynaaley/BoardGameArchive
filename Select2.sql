0b906834a10c606bd48d9d84722d790bd539fa70
Zeynep TATAR <55032271+zeynep-pixel@users.noreply.github.com>
2024-05-26 00:38:31 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -3,8 +3,24 @@
 SELECT tblUye.UYE_ID, tblUye.AD_Üye, tblUye.SOYAD_Üye , COUNT(DISTINCT tblOyuna_puan_verir.ID) as Puan_Verme_Sayisi
 FROM tblUye
 LEFT JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
-LEFT JOIN tbloyun ON tbloyun.OYUN_ID = tblKoleksiyon.OYUN_ID
 LEFT JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
 GROUP BY tblUye.UYE_ID, tblUye.AD_Üye , tblUye.SOYAD_Üye ;
+
+
+SELECT 
+    tblUye.UYE_ID, 
+    tblUye.AD_Üye, 
+    tblUye.SOYAD_Üye,   
+    COUNT(CASE WHEN tbloyun_koleksiyon.OYUN_SAHIP_OLMA_TARIHI IS NOT NULL THEN tbloyun_koleksiyon.OYUN_ASILAD END) as Oyun_Sayısı 
+FROM 
+    tblUye
+JOIN 
+    tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
+JOIN 
+    tbloyun_koleksiyon ON tblKoleksiyon.KOLEKSIYON_ID = tbloyun_koleksiyon.KOLEKSIYON_ID
+GROUP BY 
+    tblUye.UYE_ID, 
+    tblUye.AD_Üye, 
+    tblUye.SOYAD_Üye;
 
 
