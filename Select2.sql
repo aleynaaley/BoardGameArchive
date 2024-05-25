cea6d3c909e84f4cabb04b9b3a9bd666fce0aef9
Zeynep TATAR <55032271+zeynep-pixel@users.noreply.github.com>
2024-05-26 00:11:33 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -1,6 +1,6 @@
 
 
-SELECT tblUye.UYE_ID, tblUye.AD_Üye, tblUye.SOYAD_Üye , COUNT(tblOyuna_puan_verir.ID) as Puan_Verme_Sayisi
+SELECT tblUye.UYE_ID, tblUye.AD_Üye, tblUye.SOYAD_Üye , COUNT(DISTINCT tblOyuna_puan_verir.ID) as Puan_Verme_Sayisi
 FROM tblUye
 JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
 JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
