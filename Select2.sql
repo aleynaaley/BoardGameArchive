616842a8518cab8385c3835586c19d81a34908f6
Tuğba Nur Aslan <117925636+tugbanuaslan7@users.noreply.github.com>
2024-05-26 00:20:03 +0300

    Update Select2.sql

--- Select2.sql
+++ Select2.sql
@@ -2,8 +2,9 @@
 
 SELECT tblUye.UYE_ID, tblUye.AD_Üye, tblUye.SOYAD_Üye , COUNT(DISTINCT tblOyuna_puan_verir.ID) as Puan_Verme_Sayisi
 FROM tblUye
-JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
-JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
+LEFT JOIN tblKoleksiyon ON tblUye.UYE_ID = tblKoleksiyon.UYE_ID
+LEFT JOIN tbloyun ON tbloyun.OYUN_ID = tblKoleksiyon.OYUN=ID
+LEFT JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON
 GROUP BY tblUye.UYE_ID, tblUye.AD_Üye , tblUye.SOYAD_Üye ;
 
 
