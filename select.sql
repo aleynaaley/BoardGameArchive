1de7865b61f3cdef020cd8c4d1b264b9614a300f
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-05-26 21:54:41 +0300

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,54 +1,22 @@
 USE boardgame
-
-  -- SELECT 1: tüm üyelerin adı, soyadı ülkesi, sahip olduğu expansion sayısı, kaç oyuna puan vermiş, 
-  -- ortalama kaç puan vermiş, kaç oyun oturumu oluşturmuş, kaç defa ticket to ride isimli oyunu oynamış gösteren sorgu
+--1.sorgu	
 SELECT 
-    uye.AD_Üye AS 'Üye Adı',
-    uye.SOYAD_Üye AS 'Üye Soyadı',
-    ulke.ULKE_AD AS 'Ülke',
-    COUNT(DISTINCT CASE WHEN yt.Ad_Yayinturu = 'Ana Oyun' THEN y.YAYIN_ID END) AS 'Sahip Olduğu Ana Oyun Sayısı',
-	  COUNT(DISTINCT CASE WHEN yt.Ad_Yayinturu = 'Expansion' THEN y.YAYIN_ID END) AS 'Sahip Olduğu Expansion Sayısı',
-    COUNT(DISTINCT opv.OYUN) AS 'Kaç Oyuna Puan Vermiş',
-    AVG(CAST(opv.OYUN AS FLOAT)) AS 'Ortalama Puan',
-    COUNT(DISTINCT os.OYUNSEANS_ID) AS 'Oluşturduğu Oyun Oturumu Sayısı',
-    COUNT(DISTINCT CASE WHEN oyun.OYUN_ASILAD = 'Ticket to Ride' THEN uoo.OYNAR_ID END) AS 'Ticket to Ride Oynama Sayısı'
+    tblUye.AD_Üye, 
+    tblUye.SOYAD_Üye, 
+    tblUlke.ULKE_AD, 
+    Sahip_Oldugu_Ana_Oyun_Sayisi,
+    Kac_Oyuna_Puan_Vermis,
+    Ortalama_Puan,
+    Oyun_Seansi_Sayisi,
+    Ticket_to_Ride_Oynama_Sayisi
 
-FROM tblUye uye
+FROM tblUye
 
-LEFT JOIN tblUlke ulke ON uye.ULKE_ID = ulke.ULKE_ID
-LEFT JOIN tblYayin y ON uye.UYE_ID = y.UYE_ID
-LEFT JOIN tblYayinturu yt ON y.YAYINTURU_ID = yt.ID_Yayinturu
-LEFT JOIN tblOyuna_puan_verir opv ON uye.UYE_ID = opv.KOLEKSIYON
-LEFT JOIN tblOyun_seanslar os ON uye.UYE_ID = os.OLUSTURAN_UYE_ID
-LEFT JOIN tbluye_oyun_oynar uoo ON uye.UYE_ID = uoo.UYE_ID
-LEFT JOIN tblOyun oyun ON uoo.OYUNSEANS_ID = os.OYUNSEANS_ID AND os.oyun_ID = oyun.OYUN_ID
+LEFT JOIN tblUlke ON tblUye.ULKE_ID = tblUlke.ULKE_ID
+LEFT JOIN ( SELECT UYE_ID, COUNT(KOLEKSIYON_ID) AS Sahip_Oldugu_Ana_Oyun_Sayisi FROM tblKoleksiyon GROUP BY UYE_ID) AS KoleksiyonSayisi ON tblUye.UYE_ID = KoleksiyonSayisi.UYE_ID
+LEFT JOIN ( SELECT  tblKoleksiyon.UYE_ID, COUNT(DISTINCT tblOyuna_puan_verir.ID) AS Kac_Oyuna_Puan_Vermis, AVG(tblOyuna_puan_verir.PUAN) AS Ortalama_Puan FROM tblKoleksiyon LEFT JOIN tblOyuna_puan_verir ON tblKoleksiyon.KOLEKSIYON_ID = tblOyuna_puan_verir.KOLEKSIYON GROUP BY tblKoleksiyon.UYE_ID) AS PuanVermeSayisi ON tblUye.UYE_ID = PuanVermeSayisi.UYE_ID
+LEFT JOIN ( SELECT  tbluye_oyun_oynar.UYE_ID, COUNT(tbluye_oyun_oynar.OYNAR_ID) AS Oyun_Seansi_Sayisi FROM tbluye_oyun_oynar GROUP BY tbluye_oyun_oynar.UYE_ID) AS OyunSeansiSayisi ON tblUye.UYE_ID = OyunSeansiSayisi.UYE_ID
+LEFT JOIN ( SELECT  tbluye_oyun_oynar.UYE_ID, COUNT(tbluye_oyun_oynar.OYNAR_ID) AS Ticket_to_Ride_Oynama_Sayisi FROM tbluye_oyun_oynar INNER JOIN tbloyun ON tbluye_oyun_oynar.OYUNSEANS_ID = tbloyun.OYUN_ID 
+WHERE tbloyun.OYUN_ASILAD = 'Ticket to Ride' GROUP BY tbluye_oyun_oynar.UYE_ID ) AS TicketToRideOynamaSayisi ON tblUye.UYE_ID = TicketToRideOynamaSayisi.UYE_ID;
 
-GROUP BY uye.AD_Üye, uye.SOYAD_Üye, ulke.ULKE_AD;
-
-
-
-
-
-
-
----------------------
-
-USE boardgame;
-
--- Üyelerin adı, soyadı, ülkesi, sahip olduğu ana oyun sayısı, sahip olduğu eklenti paketi sayısı,
--- kaç oyuna puan verdiği, kaç oyun oturumu oluşturduğu ve kaç defa "Ticket to Ride" isimli oyunu oynadığı bilgilerini getiren sorgu.
-SELECT 
-    u.AD_Üye AS Ad,
-    u.SOYAD_Üye AS Soyad,
-    ulke.ULKE_AD AS Ulke,
-    (SELECT COUNT(*) FROM tbloyun o WHERE o.KOLEKSIYON_ID IN (SELECT k.KOLEKSIYON_ID FROM tblKoleksiyon k WHERE k.UYE_ID = u.UYE_ID) AND o.EKLENTI_PAKETI IS NULL) AS AnaOyunSayisi,
-    (SELECT COUNT(*) FROM tbloyun o WHERE o.KOLEKSIYON_ID IN (SELECT k.KOLEKSIYON_ID FROM tblKoleksiyon k WHERE k.UYE_ID = u.UYE_ID) AND o.EKLENTI_PAKETI IS NOT NULL) AS EklentiPaketiSayisi,
-    (SELECT COUNT(*) FROM tblOyuna_puan_verir opv WHERE opv.UYE_ID = u.UYE_ID) AS PuanVerdigiOyunSayisi,
-    (SELECT COUNT(*) FROM tblOyun_seanslar os WHERE os.OLUSTURAN_UYE_ID = u.UYE_ID) AS OlusturduguOyunOturumSayisi,
-    (SELECT COUNT(*) FROM tbluye_oyun_oynar uoo
-     JOIN tblOyun_seanslar os ON uoo.OYUNSEANS_ID = os.OYUNSEANS_ID
-     JOIN tbloyun o ON os.OYUN_ID = o.OYUN_ID
-     WHERE uoo.UYE_ID = u.UYE_ID AND o.OYUN_ASILAD = 'Ticket to Ride') AS TicketToRideOynamaSayisi
-FROM tblUye u
-JOIN tblUlke ulke ON u.ULKE_ID = ulke.ULKE_ID
-ORDER BY u.AD_Üye, u.SOYAD_Üye;
+--2.sorgu
