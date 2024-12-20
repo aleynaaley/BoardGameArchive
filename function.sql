e5d8354a753e2587d2f0c80275a32a5e869c1b88
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-12-20 14:44:39 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -58,3 +58,32 @@ BEGIN
     RETURN @EnCokVakitGecirilenOyunID;
 END;
 GO
+
+
+CREATE FUNCTION fncEnCokOynananOyun(@UYE_ID INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @EnCokOynananOyunID INT;
+
+    SELECT TOP 1 
+        @EnCokOynananOyunID = os.oyun_ID
+    FROM 
+    (
+        SELECT 
+            os.oyun_ID, 
+            COUNT(*) AS OynanmaSayisi
+        FROM 
+            tbluye_oyun_oynar oo
+            INNER JOIN tblOyun_seanslar os ON oo.OYUNSEANS_ID = os.OYUNSEANS_ID
+        WHERE 
+            oo.UYE_ID = @UYE_ID
+        GROUP BY 
+            os.oyun_ID
+    ) AS Oyunlar
+    ORDER BY 
+        Oyunlar.OynanmaSayisi DESC, Oyunlar.oyun_ID; -- İlk oynanma sayısına, sonra oyun_ID'ye göre sırala
+
+    RETURN @EnCokOynananOyunID;
+END
+GO
